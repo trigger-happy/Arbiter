20ea1dc77ba079058be5be0e9299961dd5be7e6c
Wilhansen Li <wil@nohakostudios.net>
2010-10-25 11:30:14 +0800

    Initial entry-point hook using OptionsParser.

--- src/runner/runner.cpp
+++ src/runner/runner.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include <apr.h>
+#include <apr_general.h>
+#include "OptionsParser.h"
+
+using namespace std;
 
 int main(int argc, char **argv) {
+	apr_app_initialize(&argc, (const char * const **)&argv, 0);
+	atexit(apr_terminate);
 
+	try{
+		OptionsParser* handler = new OptionsParser();
+		handler->setOptions(argc,argv);
+		cout << endl;
+		handler->printOptions();
+	} catch(std::exception& e) {
+		cout << e.what() << endl;
+		return 1;
+	}
 	return 0;
 }
