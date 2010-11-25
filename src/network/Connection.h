6fa7eb71178c74431ebe9daacb8af4398aace917
Thomas Dy <thatsmydoing@gmail.com>
2010-11-25 19:15:41 +0800

    Add synchronous read and write to Connection.h

--- src/network/Connection.h
+++ src/network/Connection.h
@@ -45,19 +45,17 @@ public:
     : socket_(io_service)
   {
   }
-  
+
   virtual ~Connection()
   {
   }
-  
+
   boost::asio::ip::tcp::socket& socket()
   {
     return socket_;
   }
-  
-  template <typename Handler>
-  void async_write(const NetworkMessage& message, Handler handler)
-  {
+
+  std::vector<boost::asio::const_buffer> prepareMessage(const NetworkMessage& message) {
     // Serialize the data first so we know how large it is.
     message.SerializeToString(&outbound_data_);
 
@@ -68,9 +66,7 @@ public:
     if (!header_stream || header_stream.str().size() != HEADER_LENGTH)
     {
       // Something went wrong, inform the caller.
-      boost::system::error_code error(boost::asio::error::invalid_argument);
-      socket_.io_service().post(boost::bind(handler, error));
-      return;
+      throw boost::system::system_error(boost::asio::error::invalid_argument);
     }
     outbound_header_ = header_stream.str();
 
@@ -79,23 +75,61 @@ public:
     std::vector<boost::asio::const_buffer> buffers;
     buffers.push_back(boost::asio::buffer(outbound_header_));
     buffers.push_back(boost::asio::buffer(outbound_data_));
-    boost::asio::async_write(socket_, buffers, handler);
+    return buffers;
   }
-  
+
+  template <typename Handler>
+  void async_write(const NetworkMessage& message, Handler handler)
+  {
+    try {
+      boost::asio::async_write(socket_, prepareMessage(message), handler);
+    }
+    catch(boost::system::system_error& e) {
+      socket_.io_service().post(boost::bind(handler, e.code()));
+    }
+  }
+
+  void write(const NetworkMessage& message) {
+    boost::asio::write(socket_, prepareMessage(message));
+  }
+
   template <typename Handler>
   void async_read(NetworkMessage& message, Handler handler)
   {
     // Issue a read operation to read exactly the number of bytes in a header.
-    void (Connection::*f)(
-	const boost::system::error_code&,
-	NetworkMessage&, boost::tuple<Handler>)
+    void (Connection::*f) (const boost::system::error_code&, NetworkMessage&, boost::tuple<Handler>)
       = &Connection::handle_read_header<Handler>;
-    boost::asio::async_read(socket_, boost::asio::buffer(inbound_header_),
-	boost::bind(f,
-	  this, boost::asio::placeholders::error, boost::ref(message),
-	  boost::make_tuple(handler)));
+    boost::asio::async_read(
+      socket_,
+      boost::asio::buffer(inbound_header_),
+      boost::bind(f, this, boost::asio::placeholders::error, boost::ref(message), boost::make_tuple(handler))
+    );
   }
-  
+
+  void read(NetworkMessage& message) {
+    boost::asio::read(socket_, boost::asio::buffer(inbound_header_));
+
+    std::istringstream is(std::string(inbound_header_, HEADER_LENGTH));
+    std::size_t inbound_data_size = 0;
+    if (!(is >> std::hex >> inbound_data_size))
+    {
+      // Header doesn't seem to be valid. Inform the caller.
+      throw boost::system::system_error(boost::asio::error::invalid_argument);
+    }
+    inbound_data_.resize(inbound_data_size);
+    boost::asio::read(socket_, boost::asio::buffer(inbound_data_));
+    try
+    {
+      std::string archive_data(&inbound_data_[0], inbound_data_.size());
+      message.ParseFromString(archive_data);
+    }
+    catch (std::exception& e)
+    {
+      // Unable to decode data.
+      throw boost::system::system_error(boost::asio::error::invalid_argument);
+    }
+  }
+
   template <typename Handler>
   void handle_read_header(const boost::system::error_code& e,
       NetworkMessage& message, boost::tuple<Handler> handler)
@@ -111,24 +145,23 @@ public:
       std::size_t inbound_data_size = 0;
       if (!(is >> std::hex >> inbound_data_size))
       {
-	// Header doesn't seem to be valid. Inform the caller.
-	boost::system::error_code error(boost::asio::error::invalid_argument);
-	boost::get<0>(handler)(error);
-	return;
+        // Header doesn't seem to be valid. Inform the caller.
+        boost::system::error_code error(boost::asio::error::invalid_argument);
+        boost::get<0>(handler)(error);
+        return;
       }
 
       // Start an asynchronous call to receive the data.
       inbound_data_.resize(inbound_data_size);
       void (Connection::*f)(
-	  const boost::system::error_code&,
-	  NetworkMessage&, boost::tuple<Handler>)
-	= &Connection::handle_read_data<Handler>;
-      boost::asio::async_read(socket_, boost::asio::buffer(inbound_data_),
-	boost::bind(f, this,
-	  boost::asio::placeholders::error, boost::ref(message), handler));
+      const boost::system::error_code&,
+      NetworkMessage&, boost::tuple<Handler>) = &Connection::handle_read_data<Handler>;
+        boost::asio::async_read(socket_, boost::asio::buffer(inbound_data_),
+      boost::bind(f, this,
+      boost::asio::placeholders::error, boost::ref(message), handler));
     }
   }
-  
+
   template <typename Handler>
   void handle_read_data(const boost::system::error_code& e,
       NetworkMessage& message, boost::tuple<Handler> handler)
@@ -142,26 +175,26 @@ public:
       // Extract the data structure from the data just received.
       try
       {
-	std::string archive_data(&inbound_data_[0], inbound_data_.size());
-	message.ParseFromString(archive_data);
+        std::string archive_data(&inbound_data_[0], inbound_data_.size());
+        message.ParseFromString(archive_data);
       }
       catch (std::exception& e)
       {
-	// Unable to decode data.
-	boost::system::error_code error(boost::asio::error::invalid_argument);
-	boost::get<0>(handler)(error);
-	return;
+        // Unable to decode data.
+        boost::system::error_code error(boost::asio::error::invalid_argument);
+        boost::get<0>(handler)(error);
+        return;
       }
 
       // Inform caller that data has been received ok.
       boost::get<0>(handler)(e);
     }
   }
-  
+
 private:
   /// The actual socket
   boost::asio::ip::tcp::socket socket_;
-  
+
   /// Holds an outbound header.
   std::string outbound_header_;
 
